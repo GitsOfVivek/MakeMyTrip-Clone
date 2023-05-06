ab5721458a6f268973caecbe86e9ae9bda745570
Vivek Kumar <singhvivek1999@yahoo.com>
2023-05-06 22:10:27 +0530

    width updated

--- client/src/components/Offers/Offers.jsx
+++ client/src/components/Offers/Offers.jsx
@@ -38,38 +38,62 @@ const Offers = () => {
 					</div>
 				</div>
 				<div className="sec2">
-					<OfferCard
-						img={offer1}
-						head={'DOM HOTELS'}
-						title={'Book Super Cool Stays for Your Summer Break!'}
-						para={
-							'Grab Up to 45% OFF* on Hotels & Homestays in India'
-						}
-					/>
-					<OfferCard
-						img={offer2}
-						head={'INTL FLIGHTS'}
-						title={'Welcoming Drukair to the India Skies!'}
-						para={
-							'Book flights between Delhi/Kolkata and Paro @ up to ₹2,000 OFF*.'
-						}
-					/>
-					<OfferCard
-						img={offer3}
-						head={'INTL FLIGHTS'}
-						title={'Around-The-World Travel Deals are Here!'}
-						para={'FLAT 6% OFF* on International Flights'}
-					/>
-					<OfferCard
-						img={offer4}
-						head={'CABS'}
-						title={
-							'HERE NOW: Suhana Safar Sale for Your Summer Trips!'
-						}
-						para={
-							'Enjoy delightful deals on cabs, buses & trains. Also, grab 6-7 PM Jackpot Deals.'
-						}
-					/>
+					<div>
+						<OfferCard
+							img={offer1}
+							head={'DOM HOTELS'}
+							title={
+								'Book Super Cool Stays for Your Summer Break!'
+							}
+							para={
+								'Grab Up to 45% OFF* on Hotels & Homestays in India'
+							}
+						/>
+						<OfferCard
+							img={offer2}
+							head={'INTL FLIGHTS'}
+							title={'Welcoming Drukair to the India Skies!'}
+							para={
+								'Book flights between Delhi/Kolkata and Paro @ up to ₹2,000 OFF*.'
+							}
+						/>
+						<OfferCard
+							img={offer3}
+							head={'INTL FLIGHTS'}
+							title={'Around-The-World Travel Deals are Here!'}
+							para={'FLAT 6% OFF* on International Flights'}
+						/>
+					</div>
+					<div>
+						<OfferCard
+							img={offer4}
+							head={'CABS'}
+							title={
+								'HERE NOW: Suhana Safar Sale for Your Summer Trips!'
+							}
+							para={
+								'Enjoy delightful deals on cabs, buses & trains. Also, grab 6-7 PM Jackpot Deals.'
+							}
+						/>
+						<OfferCard
+							img={offer1}
+							head={'DOM HOTELS'}
+							title={
+								'Book Super Cool Stays for Your Summer Break!'
+							}
+							para={
+								'Grab Up to 45% OFF* on Hotels & Homestays in India'
+							}
+						/>
+						<OfferCard
+							img={offer2}
+							head={'INTL FLIGHTS'}
+							title={'Welcoming Drukair to the India Skies!'}
+							para={
+								'Book flights between Delhi/Kolkata and Paro @ up to ₹2,000 OFF*.'
+							}
+						/>
+					</div>
 				</div>
 			</div>
 		</section>
