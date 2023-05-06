ce853ddadad1132c86278e1f912ef671478e176c
Vivek Kumar <singhvivek1999@yahoo.com>
2023-05-06 18:48:29 +0530

    mongoose connection according to cyclic

--- server.js
+++ server.js
@@ -5,8 +5,17 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 const DB_URI = process.env.DB_URI;
 
-mongoose.connect(DB_URI).then(() => {
-	console.log('DB connection successfull!');
+const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(DB_URI);
+		console.log(`DB connection successfull! : ${conn.connection.host}`);
+	} catch (err) {
+		console.log(err);
+		process.exit(1);
+	}
+};
+connectDB().then(() => {
+	app.listen(port, () =>
+		console.log(`Example app listening on port ${port}!`)
+	);
 });
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
