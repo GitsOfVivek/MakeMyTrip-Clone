76d33ec2d11261651c935196896b7e1f4033efcd
Vivek Kumar <singhvivek1999@yahoo.com>
2023-05-06 18:41:37 +0530

    server port change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
-const port = process.env.PORT || 2727;
+const port = process.env.PORT || 3000;
 const DB_URI = process.env.DB_URI;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB = DB_URI.replace('<PASSWORD>', DB_PASSWORD);
