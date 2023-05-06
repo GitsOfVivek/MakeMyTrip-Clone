3ada6ca040f9b96b33cc4f093e3ad0e0c793b078
Vivek Kumar <singhvivek1999@yahoo.com>
2023-05-06 19:05:22 +0530

    done

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,5 @@
-export const FLIGHT_API_LINK = 'http://localhost:2727/api/v1/flights';
-export const HOTEL_API_LINK = 'http://localhost:2727/api/v1/hotels';
-export const TRAIN_API_LINK = 'http://localhost:2727/api/v1/trains';
-
-// export const FLIGHT_API_URL =
-// 	'https://makemytrip-backend.onrender.com/api/v1/flights';
-// export const HOTEL_API_URL =
-// 	'https://makemytrip-backend.onrender.com/api/v1/hotels';
-// export const TRAIN_API_URL =
-// 	'https://makemytrip-backend.onrender.com/api/v1/trains';
+export const FLIGHT_API_URL = 'https://mmt-backend-hrc2.onrender.com/flights';
+export const HOTEL_API_URL =
+	'https://mmt-backend-hrc2.onrender.com/api/v1/hotels';
+export const TRAIN_API_URL =
+	'https://mmt-backend-hrc2.onrender.com/api/v1/trains';
--- server.js
+++ server.js
@@ -5,17 +5,8 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 const DB_URI = process.env.DB_URI;
 
-const connectDB = async () => {
-	try {
-		const conn = await mongoose.connect(DB_URI);
-		console.log(`DB connection successfull! : ${conn.connection.host}`);
-	} catch (err) {
-		console.log(err);
-		process.exit(1);
-	}
-};
-connectDB().then(() => {
-	app.listen(port, () =>
-		console.log(`Example app listening on port ${port}!`)
-	);
+mongoose.connect(DB_URI).then(conn => {
+	console.log(`DB connection successfull! : ${conn.connection.host}`);
 });
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
