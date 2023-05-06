2ef3c6420e73f350cc002dc4a0c514bfb1c039ef
Vivek Kumar <singhvivek1999@yahoo.com>
2023-05-06 19:40:49 +0530

    complete

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
-export const FLIGHT_API_URL = 'https://mmt-backend-hrc2.onrender.com/flights';
-export const HOTEL_API_URL =
+export const FLIGHT_API_LINK = 'https://mmt-backend-hrc2.onrender.com/flights';
+export const HOTEL_API_LINK =
 	'https://mmt-backend-hrc2.onrender.com/api/v1/hotels';
-export const TRAIN_API_URL =
+export const TRAIN_API_LINK =
 	'https://mmt-backend-hrc2.onrender.com/api/v1/trains';
