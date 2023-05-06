374ffe41b2122d4e3bd56dc1ffff118538ea8b9a
Vivek Kumar <singhvivek1999@yahoo.com>
2023-05-06 22:26:26 +0530

    navigate

--- client/src/components/FlightInputBox/FlightInputBox.jsx
+++ client/src/components/FlightInputBox/FlightInputBox.jsx
@@ -37,7 +37,7 @@ const FlightInputBox = ({ flights, setFilteredFlight }) => {
 
 	const searchHandler = () => {
 		if (!flights) {
-			navigate('/');
+			navigate('/flights');
 		}
 		setFilteredFlight(
 			flights.filter(
