3da0573952b36eeba80dd1901ccd865967121dbb
Vivek Kumar <singhvivek1999@yahoo.com>
2023-05-06 19:44:14 +0530

    Link typo fix

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,5 @@
-export const FLIGHT_API_LINK = 'https://mmt-backend-hrc2.onrender.com/flights';
+export const FLIGHT_API_LINK =
+	'https://mmt-backend-hrc2.onrender.com/api/v1/flights';
 export const HOTEL_API_LINK =
 	'https://mmt-backend-hrc2.onrender.com/api/v1/hotels';
 export const TRAIN_API_LINK =
